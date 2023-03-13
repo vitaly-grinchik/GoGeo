24477d3f796ef72263d136f2c8f9caa80ffe3a09
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-03-13 19:45:00 +0300

    Initial Commit

---
+++ GoGeo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoGeo
+//
+//  Created by Виталий Гринчик on 13.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
