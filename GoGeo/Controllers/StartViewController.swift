637828b62302f803c2c782c27f2ae18b6b2fc99c
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-03-29 13:43:11 +0300

    Modify

--- GoGeo/Controllers/StartViewController.swift
+++ GoGeo/Controllers/StartViewController.swift
@@ -15,7 +15,7 @@ class StartViewController: UIViewController {
     
     private let activityIndicator = UIActivityIndicatorView(style: .large)
     
-    private var counter = 0
+//    private var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +25,13 @@ class StartViewController: UIViewController {
         
         activityIndicator.startAnimating()
         
-        getChunkOfCountries { [weak self] countries, totalCount, offset, url in
-            self?.allCountries.append(contentsOf: countries)
-            self?.allCountries.forEach { print($0.name) }
+        getChunkOfCountries { countries, totalCount, offset, url in
+            self.allCountries.append(contentsOf: countries)
+            self.allCountries.forEach { print($0.name) }
             print("Left to download: \(totalCount - offset)")
             print("Next batch: \(url)")
             
-            self?.activityIndicator.stopAnimating()
+            self.activityIndicator.stopAnimating()
         }
     }
     
