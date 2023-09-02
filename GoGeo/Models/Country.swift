6cc17d22a36cbd029b4bd15d5bd8c6b1c754cc13
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-09-02 19:37:12 +0300

    Add model structs for correct JSON decoding

--- GoGeo/Models/Country.swift
+++ GoGeo/Models/Country.swift
@@ -5,29 +5,21 @@
 //  Created by Виталий Гринчик on 14.03.23.
 //
 
+
+struct CountryResponse: Decodable {
+    let data: [CountryBrief]
+}
+
+struct CountryDetailsResponse: Decodable {
+    let data: [CountryDetails]
+}
+
 // Minimal country info
 struct CountryBrief: Decodable {
     let code: String
     let currencyCodes: [String]
     let name: String
     let wikiDataId: String
-    
-    init(data: [String: Any]) {
-        code = data["code"] as? String ?? "-"
-        currencyCodes = data["currencyCodes"] as? [String] ?? []
-        name = data["name"] as? String ?? "-"
-        wikiDataId = data["wikiDataId"] as? String ?? "-"
-    }
-    
-    static func getCountries(from data: Any) -> [CountryBrief] {
-        // Check if data is JSON format compatible
-        guard let jsonData = data as? [String: Any] else { return [] }
-        // Check if JSON data has got data for key "data"
-        guard let countries = jsonData["data"] as? [[String: Any]] else { return [] }
-        
-        // [String: Any] -> [CountryBrief]
-        return countries.compactMap { CountryBrief(data: $0) }
-    }
 }
 
 // CountryDetails: Full country details
@@ -40,22 +32,4 @@ struct CountryDetails: Decodable {
     let name: String
     let numRegions: Int
     let wikiDataId: String
-    
-    init(data: [String: Any]) {
-        capital = data["capital"] as? String ?? "-"
-        code = data["code"] as? String ?? "-"
-        callingCode = data["callingCode"] as? String ?? "-"
-        currencyCodes = data["currencyCodes"] as? [String] ?? ["-"]
-        flagImageUri = data["flagImageUri"] as? String ?? "-"
-        name = data["name"] as? String ?? "-"
-        numRegions = data["numRegions"] as? Int ?? 0
-        wikiDataId = data["wikiDataId"] as? String ?? "-"
-    }
-    
-    static func getCountryDetails(from data: Any) -> CountryDetails? {
-        guard let data = data as? [String: Any] else { return nil }
-        guard let country = data["data"] as? [String: Any] else { return nil }
-        
-        return CountryDetails(data: country)
-    }
 }
